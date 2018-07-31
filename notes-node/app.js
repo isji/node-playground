c2cd864c3b5936962fa1193ee1fafbbc8dd400ea
Sj Balatan <SJ@Sjs-MacBook-Air.local>
2018-07-31 22:21:13 +0800

    initial commit

---
+++ notes-node/app.js
@@ -0,0 +1,27 @@
+console.log('Starting app.js');
+
+//const os = require('os');
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes.js');
+
+// var res = notes.addNote();
+// console.log(res);
+
+// var sum = notes.add(9,20);
+// console.log(`Result: ${sum}`);
+
+// console.log(_.isString(1));
+// console.log(_.isString('one'));
+
+// var filteredArray = _.uniq(['isji', 'len','len', 'sj', 'jasper', 'axel', 'axel']);
+// console.log(filteredArray);
+
+// var user = os.userInfo();
+// console.log(user);
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}!! \n`, function(err){
+//     if(err){
+//         console.log('Unable to write to file');
+//     }
+// });
